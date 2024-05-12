d738126fa2ccbef890b2dc0c2b6e1d2263e41720
Sayan Dhar <112699209+insvedant@users.noreply.github.com>
2024-05-12 19:37:52 +0000

    updated on sunday

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null >0);
+// console.log(null == 0);
+// console.log(null>=0);
+
+//comparisons turns null into zero thasts why first one is false where the third one is true
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+

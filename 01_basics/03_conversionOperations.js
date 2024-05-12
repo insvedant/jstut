0e43d227924ab7d0c29a0d9625349b4122cff678
Sayan Dhar <112699209+insvedant@users.noreply.github.com>
2024-05-12 21:11:50 +0000

    updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -11,11 +11,11 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 let isLoggedIn1 = "sayan"
 let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
-console.log(booleanIsLoggedIn1);
+//console.log(booleanIsLoggedIn1);
 
 //1=> true , 0=> false, ""=> false, "somethingInString"=>  true
 
@@ -23,5 +23,38 @@ let someNumber = 33
 
 let someString = String(someNumber)
 
-console.log(someString);
-console.log(typeof someString);
+//console.log(someString);
+//console.log(typeof someString);
+
+//********operations***** */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/4);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Sayan"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2+"2");
+// console.log("1"+ 2 +2);
+// console.log((1+2)+ "2");
+// console.log(((1+"2")+3)%6);
+
+let gameCounter = 10
+
+++gameCounter
+console.log(gameCounter);
+
+//prefix and postfix from js mdn site

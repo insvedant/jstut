e71da526500bcd78425a5b21dc85b71da1fc8bd5
Sayan Dhar <112699209+insvedant@users.noreply.github.com>
2024-05-12 20:37:46 +0000

    datatype summery on 13 may

---
+++ 01_basics/datatype-summery.js
@@ -0,0 +1,33 @@
+// theres mainly two types of data 1) Primitive and 2)non-primitive or reference type
+
+//theres 7 types of primitive datatypes - string, number, boolean, null, undefined, symbol, BigInt
+
+// const score = "100"
+// const scoreValue = 100.2
+// const isLoggedIn = true
+// const outsideTemp = null
+// let userEmail; // this is undefined datatype
+// const id = symbol("123")
+// const anotherId = symbol("123")
+
+// console.log(id === anotherId);
+
+// theres three types of non-Primitive or reference type datatypes
+
+//1)object 2)functions 3)array
+
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+// console.log(typeof myFunction);
+
+//number => number
+// boolean => boolean
+// string => string
+// symbol => symbol
+// null => null
+// undefined => undefined
+
+// array => Object
+// object => object
+// function => function

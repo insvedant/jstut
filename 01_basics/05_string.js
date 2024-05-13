224d6d3b315449ff84f52b793217bb39585fd182
Sayan Dhar <112699209+insvedant@users.noreply.github.com>
2024-05-13 17:10:28 +0000

    strings completed

---
+++ 01_basics/05_string.js
@@ -0,0 +1,30 @@
+const name = "sayan"
+const repoCount = "50"
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("sayan-sd-dhar")
+
+// console.log(gameName[3]);
+
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("a"));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7, 6)
+// console.log(anotherString);
+
+const newStringOne = "      sasyan     "
+// console.log(newStringOne.trim());
+
+const url = "sayan.com/sayan%20dhar"
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("sayan"))
+
+console.log(gameName.split("-"));

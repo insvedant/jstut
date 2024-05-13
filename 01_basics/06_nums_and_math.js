3283c2e61d77eb85d166ef7923c1d15e1ebe2545
Sayan Dhar <112699209+insvedant@users.noreply.github.com>
2024-05-13 18:09:04 +0000

    math and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 400
+const balance = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));// this function is very important for eCommerce website.
+
+
+// const otherNumber = 1123.346
+// console.log(otherNumber.toPrecision(4));
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(6.7));
+// console.log(Math.ceil(9.1));
+// console.log(Math.floor(8.6));
+
+// console.log(Math.min(2,5,7,1,9.5));
+// console.log(Math.max(2,6,3,6,8));
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*10)+min);
+

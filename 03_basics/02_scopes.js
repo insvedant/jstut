3dfc6a60054488d000178d7947422086428c0ea0
Sayan Dhar <112699209+insvedant@users.noreply.github.com>
2024-05-18 21:32:42 +0000

    scopes basics

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+let a = 100
+const b = 20
+var c = 30
+
+if(true){
+    let a = 10
+    const b =20
+    console.log("inner:",a);
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+//let a = 100 outside is global scope and let a = 10 is block scope
+
+

c82cfbb4bfeccfa0578faea3200c6c3ed7be7549
Sayan Dhar <112699209+insvedant@users.noreply.github.com>
2024-05-25 17:47:09 +0000

    completed until doWhile loop

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,68 @@
+//while loop
+
+// let index = 0
+
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index = index +2
+// }
+
+
+// let index = 0
+// while (index <=20) {
+//     console.log(`value of index is ${index}`);
+//     index= index +3
+// }
+
+// let index = 0 
+// while (index<=10) {
+//     console.log(`this is ${index}`);
+//     index= index + 1.5
+// }
+
+// trying with array 
+
+// let myArray= ["Batman", "Superman", "Flash"]
+
+// let arr = 0
+
+// while (arr<myArray.length) {
+//     console.log(`this is ${myArray[arr]}`);
+//     arr = arr+1
+// }
+
+
+// myArray = ["Family", "work", "responsibility"]
+
+// let arr = 0
+
+// while (arr< myArray.length) {
+//     console.log(` this is important ${myArray[arr]}`);
+//     arr = arr+1
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// do while loop 
+
+
+// let rollNumber = 1
+
+// do {
+//     console.log(`Rollnumber is ${rollNumber}`);
+//     rollNumber++
+// } while (rollNumber<=10);
+
+//special case
+
+// let rollNumberxx =11
+
+// do {
+//     console.log(`rollnumber is ${rollNumberxx}`);
+//     rollNumberxx++
+// } while (rollNumberxx<=6);
+// as the number 11 was already more than 6 so the snippet printed the 11 first and then when it didnt meet conditon it stopped the loop
+
